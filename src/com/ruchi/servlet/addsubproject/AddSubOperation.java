3473d582aad80742197b40bc57f1c4a58317c635
ruchidash <ruchidash89@gmail.com>
2014-03-20 19:43:56 +0530

    Added formatting.

--- src/com/ruchi/servlet/addsubproject/AddSubOperation.java
+++ src/com/ruchi/servlet/addsubproject/AddSubOperation.java
@@ -1,11 +1,13 @@
-package com.src.ruchi.servlet.addsubproject;
+package com.ruchi.servlet.addsubproject;
 
 public class AddSubOperation {
-	
-	public int  add(int a, int b) {
-		return a+b;
-	}
-public int sub(int a, int b) {
-	return a-b;
-}
+
+    public int add(int a, int b) {
+        return a + b;
+    }
+
+    public int sub(int a, int b) {
+        return a - b;
+    }
+
 }
