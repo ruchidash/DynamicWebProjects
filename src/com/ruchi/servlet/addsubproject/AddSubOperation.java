1cb8e65e6e260af004eb8aa82f4f30993aa44160
ruchidash <ruchidash89@gmail.com>
2014-04-01 22:42:40 +0530

    intermidiate stage

--- src/com/ruchi/servlet/addsubproject/AddSubOperation.java
+++ src/com/ruchi/servlet/addsubproject/AddSubOperation.java
@@ -2,17 +2,18 @@ package com.ruchi.servlet.addsubproject;
 
 public class AddSubOperation {
 
-    public int add(final int a, final int b) {
+    public int add(int a, int b) {
         return a + b;
     }
 
-    public int sub(final int a, final int b) {
+    public int sub(int a, int b) {
         return a - b;
     }
 
     public double sub(double a, double b) {
         return a - b;
     }
+
     public double add(double a, double b) {
         return a + b;
     }
